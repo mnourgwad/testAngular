24b56c65f6eb32da1d840d40934d3ef52620fc61
mnourgwad <mnourwad@gmail.com>
2019-04-26 23:21:23 +0200

    next

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,20 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './header/header.component';
+import { IntroComponent } from './intro/intro.component';
+import { ExpertiseComponent } from './expertise/expertise.component';
+import { EducationComponent } from './education/education.component';
+import { SkillsComponent } from './skills/skills.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    IntroComponent,
+    ExpertiseComponent,
+    EducationComponent,
+    SkillsComponent
   ],
   imports: [
     BrowserModule,
